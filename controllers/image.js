21cb025393654d910b08aae36aff33f220f7999b
jcarlorosco <jcarlorosco@gmil.com>
2019-12-27 05:25:22 +0800

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require("clarifai");
+
+//Clarifai Authentication key
+const app = new Clarifai.App({ apiKey: "218be8ece63141798e1ae5dea5abc7dd" });
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("Unable to connect to API"));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("Unable to get entries"));
+};
+
+module.exports = {
+  handleImage,
+  handleApiCall
+};
